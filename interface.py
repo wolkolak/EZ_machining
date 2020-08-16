4ce1616e1654f9bc01bc0b5c1c79a8cd5435ebf9
wolkolak <wolkolak1@gmail.com>
2020-08-16 23:02:39 +0400

    перешёл на pyqt5 с tikinter

--- interface.py
+++ interface.py
@@ -2,6 +2,38 @@ from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QGridLayout, QTextEdit, 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from main import settings
+
+
+
+class CenterWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        centr_grid = QGridLayout()
+        self.setLayout(centr_grid)
+        self.setStyleSheet("background-color: gray")
+
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.setStyleSheet('background-color:green')
+
+        centr_grid.addWidget(splitter)
+
+        self.left = QWidget()
+        self.left.setStyleSheet("background-color: cyan")
+        splitter.addWidget(self.left)
+
+        self.right = QWidget()
+        self.right.setStyleSheet("background-color: yellow")
+        splitter.addWidget(self.right)
+
+        self.textEdit = QTextEdit(self)
+        grid_right = QGridLayout()
+        self.right.setLayout(grid_right)
+        grid_right.addWidget(self.textEdit, 0, 0)
+
+        splitter.setSizes([100, 200])
+
 class Example(QMainWindow):
 
     def __init__(self):
@@ -9,8 +41,7 @@ class Example(QMainWindow):
 
         self.initUI()
 
-    def ololo(self):
-        print('ololo')
+
 
     def initUI(self):
 
@@ -22,11 +53,7 @@ class Example(QMainWindow):
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
         self.statusBar()
-        centr = QWidget()
-        centr_grid = QGridLayout()
-        centr.setLayout(centr_grid)
-        self.setCentralWidget(centr)
-        centr.setStyleSheet("background-color: gray")
+
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
@@ -36,30 +63,11 @@ class Example(QMainWindow):
         self.toolbar = self.addToolBar('Exit')
         self.toolbar.addAction(exitAction)
 
-
-
         self.setGeometry(100, 100, settings['main_width'], settings['main_height'])
         self.setWindowTitle('EZ machining')
 
+        centre = CenterWindow()
+        self.setCentralWidget(centre)
 
-        splitter = QSplitter(Qt.Horizontal)
-        splitter.setStyleSheet('background-color:green')
-
-        centr_grid.addWidget(splitter)
-
-
-
-        self.left = QWidget()
-        self.left.setStyleSheet("background-color: cyan")
-        splitter.addWidget(self.left )
-
-        self.right = QWidget()
-        self.right.setStyleSheet("background-color: yellow")
-        splitter.addWidget(self.right )
-
-        self.textEdit = QTextEdit(self)
-        grid_right = QGridLayout()
-        self.right.setLayout(grid_right)
-        grid_right.addWidget(self.textEdit, 0, 0)
-
-        splitter.setSizes([100, 200])
+    def ololo(self):
+        print('ololo')
